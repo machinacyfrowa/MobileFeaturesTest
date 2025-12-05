1d51a707b3d23c319d902b5af518c1fcf6a53957
Paweł Zawadzki <admin@machinacyfrowa.pl>
2025-12-05 10:30:38 +0100

    Dodano obsługę GPS i uprawnienia lokalizacji
    W pliku `GPS.xaml` dodano etykiety do wyświetlania współrzędnych GPS oraz przycisk do pobierania lokalizacji.
    W pliku `GPS.xaml.cs` zaimplementowano metodę `Start_GPS`, która żąda uprawnień do lokalizacji, pobiera współrzędne i aktualizuje etykiety.
    W pliku `AndroidManifest.xml` dodano wymagane uprawnienia do lokalizacji (`ACCESS_BACKGROUND_LOCATION`, `ACCESS_COARSE_LOCATION`, `ACCESS_FINE_LOCATION`).
    Usunięto atrybut `android:roundIcon` z elementu `<application>`.

--- MobileFeaturesTest/GPS.xaml.cs
+++ MobileFeaturesTest/GPS.xaml.cs
@@ -6,4 +6,25 @@ public partial class GPS : ContentPage
 	{
 		InitializeComponent();
 	}
+
+    async private void Start_GPS(object sender, EventArgs e)
+    {
+        //poproœ / sprawdŸ uprawnienia do lokalizacji
+        var status = await Permissions.RequestAsync<Permissions.LocationWhenInUse>();
+        if (status != PermissionStatus.Granted)
+        {
+            //jeœli nie mamy uprawnieñ, to poka¿ komunikat i zakoñcz
+            await DisplayAlert("Brak uprawnieñ", "Aplikacja nie ma uprawnieñ do lokalizacji", "OK");
+            return;
+        }
+        //teraz dopiero faktycznie pobierz lokalizacjê
+        var location = await Geolocation.Default.GetLocationAsync();
+        if (location != null)
+        {
+            LatitudeLabel.Text = "Szerokoœæ: " + location.Latitude;
+            LongitudeLabel.Text = "D³ugoœæ: " + location.Longitude;
+        }
+
+
+    }
 }
